a25c538888b576315cf3928ebbda76699e3abe90
Raven Koehler <ravenkoehler@gmail.com>
2020-07-29 19:44:51 -0400

    signup route working

--- e_commerce_app/app/controllers/users_controller.rb
+++ e_commerce_app/app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :signup
+    erb :'users/signup'
   end
 
   post '/signup' do
-    if !params.values[1..-1].include?("") && params[user[password]] == params[confirm_password]
+    if !params.values[1..-1].include?("") && params[:user][:password] == params[:confirm_password]
       User.create(params[:user])
+      "you have successfully created an account!"
       # redirect to '/users...'
     else
       redirect to '/signup'
