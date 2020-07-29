6385e1bc45fbd9ebaffecfb2d5e37e3576c88810
Raven Koehler <ravenkoehler@gmail.com>
2020-07-29 14:57:44 -0400

    added post signup route

--- e_commerce_app/app/controllers/users_controller.rb
+++ e_commerce_app/app/controllers/users_controller.rb
@@ -5,7 +5,13 @@ class UsersController < ApplicationController
   end
 
   post '/signup' do
-    
+    if !params.values[1..-1].include?("") && params[user[password]] == params[confirm_password]
+      User.create(params[:user])
+      # redirect to '/users...'
+    else
+      redirect to '/signup'
+      # add error message here
+    end
   end
 
 end
