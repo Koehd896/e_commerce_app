21b27896707253e38c834be4c336d66341ff91bd
Raven Koehler <ravenkoehler@gmail.com>
2020-07-29 15:59:25 -0400

    added seed file

--- e_commerce_app/app/controllers/application_controller.rb
+++ e_commerce_app/app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get "/" do
-    erb :welcome
+    @products = Product.all
+    erb :index
   end
 
 end
---
+++ e_commerce_app/db/seeds.rb
@@ -0,0 +1,6 @@
+n = 1
+description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+15.times do
+  Product.create(name: "item #{n}", price = n + 0.99, description: description)
+  n += 1
+end
