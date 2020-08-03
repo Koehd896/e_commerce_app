a2dbc55b8a0302ffb20a417cf19fe511142a3a47
Raven Koehler <ravenkoehler@gmail.com>
2020-08-03 12:07:10 -0400

    fixed product_table relationships, working on cart checkout

--- e_commerce_app/app/controllers/carts_controller.rb
+++ e_commerce_app/app/controllers/carts_controller.rb
@@ -19,13 +19,24 @@ class CartsController < ApplicationController
   # end
 
   patch '/users/:id/cart' do
-    cart_id = Cart.find_by(user_id: params[:id]).id
+    cart = Cart.find_by(user_id: params[:id])
     if params[:delete_product_id]
       product_id = Product.find(params[:delete_product_id]).id
-      ProductCart.create(product_id: product_id, cart_id: cart_id)
-    else
+      ProductCart.create(product_id: product_id, cart_id: cart.id)
+    elsif params[:add_product_id]
       product_id = Product.find(params[:add_product_id]).id
-      ProductCart.create(product_id: product_id, cart_id: cart_id)      
+      ProductCart.create(product_id: product_id, cart_id: cart.id)    
+    elsif params[:checkout]
+      cart.products.each do |product|
+        users = product.carts.map do |cart|
+          cart.user
+        end
+        users.each
+
+
+        product_cart = ProductCart.find_by(product_id: product.id)
+        product_cart.destroy
+      end
     end
     redirect to "/users/#{params[:id]}/cart"
   end
