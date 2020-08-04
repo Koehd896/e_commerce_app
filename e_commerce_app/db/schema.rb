9e5b6d94b7c09c4ec6c7192d2428c8242007720a
Raven Koehler <ravenkoehler@gmail.com>
2020-08-04 11:25:54 -0400

    added img attribute for product, need to test

--- e_commerce_app/db/schema.rb
+++ e_commerce_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200803173820) do
+ActiveRecord::Schema.define(version: 20200804151421) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20200803173820) do
     t.string  "password_digest"
     t.integer "cart_id"
     t.decimal "balance"
+    t.string  "img"
   end
 
 end
