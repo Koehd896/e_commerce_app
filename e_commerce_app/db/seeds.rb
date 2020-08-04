774410229839944b26a33fb729ec159490f00443
Raven Koehler <ravenkoehler@gmail.com>
2020-08-04 18:18:02 -0400

    added picture for new test product

--- e_commerce_app/db/seeds.rb
+++ e_commerce_app/db/seeds.rb
@@ -1,6 +1,11 @@
-n = 1
-description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-15.times do
-  Product.create(name: "item #{n}", price: n + 0.99, description: description)
-  n += 1
+description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
+test_user = User.create(name: "test user", username: "test user name", email: "blah@test.com", balance: 0)
+
+products = [
+  ["iMac", 600, "images/iMac.jpeg"]
+]
+
+products.each do |product|
+  Product.create(name: product[0], price: product[1], description: description, image: product[2], user: test_user)
 end
